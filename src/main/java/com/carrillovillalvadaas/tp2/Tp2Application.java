d3fd5f1949000a5e7b5f4ae57e10eebc2534aac8
Elias Villalva <villalva_00@hotmail.com>
2026-09-04 00:39:01 -0300

    Creacion del proyecto TP2 con spring boot

---
+++ src/main/java/com/carrillovillalvadaas/tp2/Tp2Application.java
@@ -0,0 +1,14 @@
+package com.carrillovillalvadaas.tp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2Application {
+
+    public static void main(String[] args) {
+        System.out.println("Holaaa");
+        SpringApplication.run(Tp2Application.class, args);
+    }
+
+}
